368bc6367fa75953303e1767c3e9da9a1424f1b7
Sigureya <sigureya1208@gmail.com>
2025-01-26 09:00:40 +0900

    初期化

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/index.ts", // ライブラリのエントリポイント
+      name: "rmmzCommandMapper", // ライブラリの名前（UMD形式用）
+      fileName: (format) => `rmmzCommandMapper.${format}.js`, // 出力ファイル名
+      formats: ["es", "cjs"], // 出力形式
+    },
+    rollupOptions: {
+      external: ["@sigureya/rpgtypes"], // 外部依存関係を適切に設定
+    },
+    sourcemap: true, // ソースマップの生成
+    outDir: "dist", // 出力先ディレクトリ
+    emptyOutDir: true, // 出力先をビルド前にクリア
+  },
+  plugins: [
+    dts({
+      include: ["src/**/*.ts"], // 型定義を生成する対象
+      exclude: ["src/**/*.test.ts"], // 型定義生成からテストコードを除外
+      insertTypesEntry: true, // 型ファイルへのエントリを追加
+    }),
+  ],
+});
